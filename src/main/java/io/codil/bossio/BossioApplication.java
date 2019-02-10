dfd631a6759dfcae0cbb0413359e31194de4eadf
Jaime <jaimitorojas@gmail.com>
2019-02-10 02:18:39 -0400

    set root context path and re-add SpringBootServletInitializer

--- src/main/java/io/codil/bossio/BossioApplication.java
+++ src/main/java/io/codil/bossio/BossioApplication.java
@@ -2,9 +2,10 @@ package io.codil.bossio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public  class BossioApplication {
+public  class BossioApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BossioApplication.class, args);
