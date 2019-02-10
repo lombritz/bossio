ecc44d72e559ed3f1da8962c648b990037bac234
Jaime <jaimitorojas@gmail.com>
2019-02-10 14:14:53 -0400

    Convert property model to db entity

--- src/main/java/io/codil/bossio/model/Property.java
+++ src/main/java/io/codil/bossio/model/Property.java
@@ -3,9 +3,22 @@ package io.codil.bossio.model;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Data
 public class Property {
-    @NonNull private Long id;
-    @NonNull private String name;
-    @NonNull private Address address;
+
+    @Id
+    @GeneratedValue
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private Address address;
 }
