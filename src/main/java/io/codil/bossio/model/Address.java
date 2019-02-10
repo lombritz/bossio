046e3f99034b73d52e394393aa92e8fc47e49d81
Jaime <jaimitorojas@gmail.com>
2019-02-10 14:23:28 -0400

    Convert address model to db entity

--- src/main/java/io/codil/bossio/model/Address.java
+++ src/main/java/io/codil/bossio/model/Address.java
@@ -1,15 +1,30 @@
 package io.codil.bossio.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Data
 public class Address {
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String number;
+
     private String street;
+
     private String postalCode;
+
     private String city;
+
     private String country;
+
     private Double latitude;
+
     private Double longitude;
 }
