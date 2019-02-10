7e579e20e5cd8ca4e5e02b6a367676ed817aad4a
Jaime <jaimitorojas@gmail.com>
2019-02-10 02:08:15 -0400

    remove SpringBootServletInitializer

--- src/main/java/io/codil/bossio/BossioApplication.java
+++ src/main/java/io/codil/bossio/BossioApplication.java
@@ -2,10 +2,9 @@ package io.codil.bossio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public  class BossioApplication extends SpringBootServletInitializer{
+public  class BossioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BossioApplication.class, args);
